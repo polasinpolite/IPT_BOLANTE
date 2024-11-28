121d8fee53e00176dc3e6493022f15a846521d1b
polasinpolite <justinpaulbolante26@gmail.com>
2024-11-28 21:29:26 +0800

    IPT_ACTIVTY

---
+++ IPT_BOLANTE/docs/js/script.js
@@ -0,0 +1,10 @@
+function addNumbers() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 + num2;
+    document.getElementById("result").innerText = `Result: ${result}`;
+}
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
